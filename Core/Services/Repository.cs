13c9d93241e24277553ff73de22f24d0a9719537
Melnik Galina <melnikgg@gmail.com>
2022-10-08 16:39:36 +0300

    merge

--- Core/Services/Repository.cs
+++ Core/Services/Repository.cs
@@ -26,25 +26,17 @@ public class Repository : IRepository
         try
         {
             var owner = ownerData.Adapt<Owner>();
-<<<<<<< HEAD
+
             //owner.Flat = ownerData.Adapt<OwnerFlat>();
             //await context.Owners.AddAsync(ownerData.Adapt<Owner>());
             //await context.SaveChangesAsync();
-=======
-            owner.Flat = ownerData.Adapt<OwnerFlat>();
-            await context.Owners.AddAsync(ownerData.Adapt<Owner>());
             result = true;
->>>>>>> 26620bbd209e7b8035fa31f2823442b3a822a169
         }
         catch (DbUpdateException ex)
         {
             _logger.LogInformation(ex, $"Owner exists {ownerData.Name}");
         }
-<<<<<<< HEAD
-=======
-        await context.SaveChangesAsync();
         return result;
->>>>>>> 26620bbd209e7b8035fa31f2823442b3a822a169
     }
 
     public async Task<bool> IsOwnerExist(string ownerName)
